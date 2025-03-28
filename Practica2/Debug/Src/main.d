9fef09ec0a8abc1ae16804250b477fa0a9e227c5
marcos1985h <ruben.huata@gmail.com>
2025-03-27 22:01:55 -0300

    Add files via upload

---
+++ Practica2/Debug/Src/main.d
@@ -0,0 +1,76 @@
+Src/main.o: ../Src/main.c ../Inc/main.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/core_cm4.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma2d.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_eth.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sdram.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_fmc.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ /home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/BSP/STM32F4xx_Nucleo_144/stm32f4xx_nucleo_144.h
+../Inc/main.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/core_cm4.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/cmsis_version.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma2d.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_eth.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sdram.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_fmc.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+/home/marcos/Documentos/PdM/PdM_workspace/Practica2/Drivers/BSP/STM32F4xx_Nucleo_144/stm32f4xx_nucleo_144.h:
